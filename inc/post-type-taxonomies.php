77d6ed050efa70767d8b0feaa28834288b50871d
Ahmadi-Najmeh <nahmadi10@my.bcit.ca>
2025-03-23 11:21:05 -0700

    Staff CPT is created. The "Add Title" is replaced. Two custom is created for email and job title and is locked.

--- inc/post-type-taxonomies.php
+++ inc/post-type-taxonomies.php
@@ -1,5 +1,6 @@
 <?php
 function school_demo_register_custom_post_types() {
+    //registering student custom post type
     $labels = array(
         'name'                     => _x( 'Students', 'post type general name', 'School-Theme' ),
         'singular_name'            => _x( 'Student', 'post type singular name', 'School-Theme' ),
@@ -66,6 +67,72 @@ function school_demo_register_custom_post_types() {
         'template_lock'      => 'all'  //preventing user from moving/ removing theme.
     );
     register_post_type( 'fwd-student', $args );
+
+    //registering stuff custom post type
+    $labels = array(
+        'name'                     => _x( 'Staff', 'post type general name', 'School-Theme' ),
+        'singular_name'            => _x( 'Staff', 'post type singular name', 'School-Theme' ),
+        'add_new'                  => _x( 'Add New', 'staff', 'School-Theme' ),
+        'add_new_item'             => __( 'Add New Staff', 'School-Theme' ),
+        'edit_item'                => __( 'Edit Staff', 'School-Theme' ),
+        'new_item'                 => __( 'New Staff', 'School-Theme' ),
+        'view_item'                => __( 'View Staff', 'School-Theme' ),
+        'view_items'               => __( 'View Staff', 'School-Theme' ),
+        'search_items'             => __( 'Search Staff', 'School-Theme' ),
+        'not_found'                => __( 'No staff found.', 'School-Theme' ),
+        'not_found_in_trash'       => __( 'No staff found in Trash.', 'School-Theme' ),
+        'parent_item_colon'        => __( 'Parent Staff:', 'School-Theme' ),
+        'all_items'                => __( 'All Staff', 'School-Theme' ),
+        'archives'                 => __( 'Staff Archives', 'School-Theme' ),
+        'attributes'               => __( 'Staff Attributes', 'School-Theme' ),
+        'insert_into_item'         => __( 'Insert into staff', 'School-Theme' ),
+        'uploaded_to_this_item'    => __( 'Uploaded to this staff', 'School-Theme' ),
+        'featured_image'           => __( 'Staff featured image', 'School-Theme' ),
+        'set_featured_image'       => __( 'Set staff featured image', 'School-Theme' ),
+        'remove_featured_image'    => __( 'Remove staff featured image', 'School-Theme' ),
+        'use_featured_image'       => __( 'Use as featured image', 'School-Theme' ),
+        'menu_name'                => _x( 'Staff', 'admin menu', 'School-Theme' ),
+        'filter_items_list'        => __( 'Filter staff list', 'School-Theme' ),
+        'items_list_navigation'    => __( 'Staff list navigation', 'School-Theme' ),
+        'items_list'               => __( 'Staff list', 'School-Theme' ),
+        'item_published'           => __( 'Staff published.', 'School-Theme' ),
+        'item_published_privately' => __( 'Staff published privately.', 'School-Theme' ),
+        'item_reverted_to_draft'  => __( 'Staff reverted to draft.', 'School-Theme' ),
+        'item_trashed'             => __( 'Staff trashed.', 'School-Theme' ),
+        'item_scheduled'           => __( 'Staff scheduled.', 'School-Theme' ),
+        'item_updated'             => __( 'Staff updated.', 'School-Theme' ),
+        'item_link'                => __( 'Staff link.', 'School-Theme' ),
+        'item_link_description'    => __( 'A link to a staff.', 'School-Theme' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staff' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-groups',
+        'supports'           => array( 'title', 'editor', 'thumbnail' ), //thumbnail is for featured images
+        'template'           => array(
+            array( 'core/paragraph', array(
+                'placeholder' => 'Job title...'
+            ) ),
+            array('core/paragraph', array(
+                'placeholder' => 'Email...'
+            ) ),
+            ),
+        'template_lock'      => 'all'  //preventing user from moving/ removing theme.
+    );
+    register_post_type( 'fwd-staff', $args );
 }
 add_action( 'init', 'school_demo_register_custom_post_types' );
 
